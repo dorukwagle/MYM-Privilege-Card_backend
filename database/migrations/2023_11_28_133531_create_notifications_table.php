c4b4a5508932ac33d61cfa3fe52ad6344a0c4322
dorukwagle <dorukwagle@gmail.com>
2023-11-28 20:21:12 +0545

    change constraints of notification table

--- database/migrations/2023_11_28_133531_create_notifications_table.php
+++ database/migrations/2023_11_28_133531_create_notifications_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->boolean('read');
-            $table->integer("post_id");
-            $table->integer("user_id");
+            $table->boolean('read')->default(0);
+            $table->integer("post_id")->nullable();
+            $table->integer("user_id")->nullable();
         });
     }
 
