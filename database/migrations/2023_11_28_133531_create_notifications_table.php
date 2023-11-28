51cd0fdc8ae95fe848b99c327a7544e809f8b236
dorukwagle <dorukwagle@gmail.com>
2023-11-28 19:46:53 +0545

    add table columns into notification migration

--- database/migrations/2023_11_28_133531_create_notifications_table.php
+++ database/migrations/2023_11_28_133531_create_notifications_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->boolean('read');
+            $table->integer("post_id");
+            $table->integer("user_id");
         });
     }
 
